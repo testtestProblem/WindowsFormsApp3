2c098d97a11f3334b9120cd109a5f670eda69631
testtestbrain <49505066+testtestbrain@users.noreply.github.com>
2023-06-01 19:04:09 +0800

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.testCommand1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU", 20F);
+            this.button1.Location = new System.Drawing.Point(28, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open serial port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 20F);
+            this.label1.Location = new System.Drawing.Point(23, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "unknow";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("PMingLiU", 20F);
+            this.button2.Location = new System.Drawing.Point(28, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "check battery";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 10F);
+            this.label2.Location = new System.Drawing.Point(25, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "receive data";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 150;
+            this.comboBox1.DropDownWidth = 50;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM0",
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11"});
+            this.comboBox1.Location = new System.Drawing.Point(229, 43);
+            this.comboBox1.MaxDropDownItems = 12;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(63, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "COM0";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 10F);
+            this.label3.Location = new System.Drawing.Point(226, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "com port";
+            // 
+            // testCommand1
+            // 
+            this.testCommand1.Location = new System.Drawing.Point(201, 90);
+            this.testCommand1.Name = "testCommand1";
+            this.testCommand1.Size = new System.Drawing.Size(75, 23);
+            this.testCommand1.TabIndex = 6;
+            this.testCommand1.Text = "test";
+            this.testCommand1.UseVisualStyleBackColor = true;
+            this.testCommand1.Click += new System.EventHandler(this.testCommand1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "battery index :??";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("PMingLiU", 20F);
+            this.label6.Location = new System.Drawing.Point(23, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 27);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "unknow";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 485);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.testCommand1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button testCommand1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort My_SerialPort;
+        private bool Console_receiving = false;
+        private Thread t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 接收資料
+        private void DoReceive()
+        {
+            Byte[] buffer = new Byte[32];
+
+            try
+            {
+                while (Console_receiving)
+                //while (My_SerialPort.DataReceived)
+                {
+                    if (My_SerialPort.BytesToRead > 0)
+                    {
+                       
+                        Int32 length = My_SerialPort.Read(buffer, 0, buffer.Length);
+
+                        string buf = Encoding.ASCII.GetString(buffer);
+                        //object p = Convert.ToHexString(buffer);
+                        label1.Text += buf;
+                        label6.Text += string.Join(" ", buffer);
+                        label6.Text += "\n";
+                        /*
+                        Array.Resize(ref buffer, length);
+                        Display d = new Display(ConsoleShow);
+                        this.Invoke(d, new Object[] { buf });
+                        Array.Resize(ref buffer, 1024);
+                        */
+                    }
+
+                    //Thread.Sleep(20);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Console 發送資料
+        public void SendData(Object sendBuffer)
+        {
+            if (sendBuffer != null)
+            {
+                Byte[] buffer = sendBuffer as Byte[];
+
+                try
+                {
+                    My_SerialPort.Write(buffer, 0, buffer.Length);
+                }
+                catch (Exception ex)
+                {
+                    CloseComport();
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //關閉 Console
+        public void CloseComport()
+        {
+            try
+            {
+                My_SerialPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Console_receiving = false;
+            t.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            My_SerialPort = new SerialPort();
+
+            if (My_SerialPort.IsOpen)
+            {
+                CloseComport();
+            }
+
+            //設定 Serial Port 參數
+            My_SerialPort.PortName = comboBox1.Text;
+            My_SerialPort.BaudRate = 9600;
+            My_SerialPort.DataBits = 8;
+            My_SerialPort.StopBits = StopBits.One;
+
+            if (!My_SerialPort.IsOpen)
+            {
+                int error = 0;
+
+                //開啟 Serial Port
+                try
+                {
+                    My_SerialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    error = 1;
+                    MessageBox.Show(ex.Message);
+                }
+
+                if (error == 0)
+                {
+                    Console_receiving = true;
+
+                    //開啟執行續做接收動作
+                    t = new Thread(DoReceive);
+                    t.IsBackground = true;
+                    t.Start();
+
+                    testCommand();
+                }
+            }
+        }
+
+        public void testCommand()
+        {
+            try
+            {
+                Byte[] buffer = new Byte[4] { 0x04, 0xA0, 0x00, 0x5c };
+                My_SerialPort.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                CloseComport();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        Byte batteryIndex =0, batteryIndexCks=0x1b;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                Byte[] buffer = new Byte[5] {/*Length*/ 0x05,/*Cmd*/ 0xB0,/*index*/ 0x30,/*battery*/ batteryIndex, batteryIndexCks };
+               My_SerialPort.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                CloseComport();
+                MessageBox.Show(ex.Message);
+            }
+
+            label4.Text = "battery index: " + batteryIndex.ToString();
+
+            batteryIndex++;
+            batteryIndexCks--;
+
+            if (batteryIndex == 4)
+            {
+                batteryIndex = 0;
+                batteryIndexCks = 0x1b;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label6.Text = "";
+        }
+
+        private void testCommand1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Byte[] buffer = new Byte[4] { 0x04, 0xA0, 0x01, 0x5b };
+                My_SerialPort.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                CloseComport();
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
